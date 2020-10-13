efc3026d466c8bf107efd163bf808711d3ac227a
drewshroyer <drewshroyer@gmail.com>
2020-10-13 16:01:47 -0400

    updates to css modal

---
+++ src/scripts/canvas/initDraw.js
@@ -0,0 +1,56 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+function initDraw(canvas) {
+  let mouse = {
+    x: 0,
+    y: 0,
+    startX: 0,
+    startY: 0,
+  };
+  function setMousePosition(e) {
+    let ev = e || window.event; //Moz || IE
+    if (ev.pageX) {
+      //Moz
+      mouse.x = ev.pageX + window.pageXOffset;
+      mouse.y = ev.pageY + window.pageYOffset;
+    } else if (ev.clientX) {
+      //IE
+      mouse.x = ev.clientX + document.body.scrollLeft;
+      mouse.y = ev.clientY + document.body.scrollTop;
+    }
+  }
+
+  let element = null;
+  canvas.onmousemove = function (e) {
+    setMousePosition(e);
+    if (element !== null) {
+      element.style.width = Math.abs(mouse.x - mouse.startX) + "px";
+      element.style.height = Math.abs(mouse.y - mouse.startY) + "px";
+      element.style.left =
+        mouse.x - mouse.startX < 0 ? mouse.x + "px" : mouse.startX + "px";
+      element.style.top =
+        mouse.y - mouse.startY < 0 ? mouse.y + "px" : mouse.startY + "px";
+    }
+  };
+
+  canvas.onclick = function (e) {
+    if (element !== null) {
+      element = null;
+      canvas.style.cursor = "default";
+      console.log("finsihed.");
+    } else {
+      console.log("begun.");
+      mouse.startX = mouse.x;
+      mouse.startY = mouse.y;
+      element = document.createElement("div");
+      element.className = "rectangle";
+      element.style.left = mouse.x + "px";
+      element.style.top = mouse.y + "px";
+      canvas.appendChild(element);
+      canvas.style.cursor = "crosshair";
+    }
+  };
+}
+
+initDraw(canvas);
---
+++ src/scripts/canvas/simpleDraw.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    let painting = false;
+    
+    function startPosition() {
+        painting = true;
+    }
+
+    function finishPosition() {
+      painting = false;
+      ctx.beginPath();
+    }
+
+    function draw(e) {
+        if (!painting) return;
+        ctx.lineWidth = 5;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = "red";
+
+        cts.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishPosition);
+    canvas.addEventListener("mousemove", draw);
+
+});
