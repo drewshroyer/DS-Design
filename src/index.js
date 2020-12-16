3dcd4527f7b96ef728e89656d4782cda5d4129ff
drewshroyer <drewshroyer@gmail.com>
2020-12-16 16:31:44 -0600

    cleaning up text

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import sidebarData from "./scripts/util/sidebar_data";
 import DrawCanvas from "./scripts/canvas/canvas";
 
 window.addEventListener("DOMContentLoaded", (main) => {
-//   const canvas = document.getElementById("canvas");
-//   const ctx = canvas.getContext("2d");
 
   const canvas = document.getElementById("drawCanvas");
   const drawCanvas = new DrawCanvas(canvas);
@@ -22,34 +20,28 @@ window.addEventListener("DOMContentLoaded", (main) => {
   canvas.width = window.innerWidth;
 
   // let painting = false;
-
   // function startPosition() {
   //   painting = true;
   // }
-
   // function finishPosition() {
   //   painting = false;
   //   ctx.beginPath();
   // }
-
   // function scribble(e) {
   //   if (!painting) return;
   //   ctx.lineWidth = 2;
   //   ctx.lineCap = "round";
   //   ctx.strokeStyle = "blue";
-
   //   ctx.lineTo(e.clientX, e.clientY);
   //   ctx.stroke();
   //   ctx.beginPath();
   //   ctx.moveTo(e.clientX, e.clientY);
   // }
-
   // canvas.addEventListener("mousedown", startPosition);
   // canvas.addEventListener("mouseup", finishPosition);
   // canvas.addEventListener("mousemove", scribble);
 
-//    const canvas = document.getElementById("canvas");
-//    const ctx = canvas.getContext("2d");
+
    let modal = document.getElementById("myModal");
    let btn = document.getElementById("myBtn");
    let close = document.getElementsByClassName("close")[0];
