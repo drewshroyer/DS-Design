25fe0019dca11cfc08b3608f935b1ff2fe79aa17
drewshroyer <drewshroyer@gmail.com>
2020-12-18 10:51:04 -0600

    cleaning up old code

--- src/scripts/canvas/canvas.js
+++ src/scripts/canvas/canvas.js
@@ -8,21 +8,15 @@ import Canvg from 'canvg';
 class MyCanvas {
   constructor(canvasElement) {
     this.canvasElement =  canvasElement;
-    this.centerPosition = this.getCenterPosition();
     this.strokeColor = 'black';
     this.fillColor = "white";
     this.defaultSize = [100,100];
     this.currentActiveItem = null;
     this.strokeWidth = 2;
-    // sets up paper js on canvas
     paper.setup(canvasElement);
-    //creates new project in paper
     this.project = new Project(canvasElement)
-    //canvas scale value
     this.canvasScaleValue = 1;
-    //creating tool
     this.tool = new Tool();
-    // has moved at least 10 points:
     tool.minDistance = 2;
 
 
@@ -45,9 +39,6 @@ class MyCanvas {
     this.drawFirePlace = this.drawFirePlace.bind(this);
     this.drawStairs = this.drawStairs.bind(this);
     this.drawSofa = this.drawSofa.bind(this);
-
-    //general method binding
-    this.getCenterPosition = this.getCenterPosition.bind(this);
 
     // anchor binding
     this.addAnchor = this.addAnchor.bind(this);
@@ -169,6 +160,8 @@ class MyCanvas {
         y: 200,
         draggable: true,
     });
+    let tr = new Konva.Transformer();
+
     // let layer = new Layer();
     // let stage = new Stage({
     //   container: 'container',
@@ -177,19 +170,121 @@ class MyCanvas {
     // });
 
     stage.add(layer);
+    layer.add(tr);
     layer.add(group);
     group.add(queenKonvaImg);
 
-    this.addAnchor(group, 0, 0, 'topLeft');
-    this.addAnchor(group, 61.9875776, 0, 'topRight');
-    this.addAnchor(group, 61.9875776, 80, 'bottomRight');
-    this.addAnchor(group, 0, 80, 'bottomLeft');
+    // this.addAnchor(group, 0, 0, 'topLeft');
+    // this.addAnchor(group, 61.9875776, 0, 'topRight');
+    // this.addAnchor(group, 61.9875776, 80, 'bottomRight');
+    // this.addAnchor(group, 0, 80, 'bottomLeft');
 
     queenImg.onload = function() {
       queenKonvaImg.image(queenImg)
       layer.draw();
     }
     queenImg.src = "src/images/queen-bed.svg";
+
+     let selectionRectangle = new Konva.Rect({
+        fill: 'rgba(0,0,255,0.5)',
+      });
+      layer.add(selectionRectangle);
+
+      let x1, y1, x2, y2;
+      stage.on('mousedown touchstart', (e) => {
+        // do nothing if we mousedown on eny shape
+        if (e.target !== stage) {
+          return;
+        }
+        x1 = stage.getPointerPosition().x;
+        y1 = stage.getPointerPosition().y;
+        x2 = stage.getPointerPosition().x;
+        y2 = stage.getPointerPosition().y;
+
+        selectionRectangle.visible(true);
+        selectionRectangle.width(0);
+        selectionRectangle.height(0);
+        layer.draw();
+      });
+
+      stage.on('mousemove touchmove', () => {
+        // no nothing if we didn't start selection
+        if (!selectionRectangle.visible()) {
+          return;
+        }
+        x2 = stage.getPointerPosition().x;
+        y2 = stage.getPointerPosition().y;
+
+        selectionRectangle.setAttrs({
+          x: Math.min(x1, x2),
+          y: Math.min(y1, y2),
+          width: Math.abs(x2 - x1),
+          height: Math.abs(y2 - y1),
+        });
+        layer.batchDraw();
+      });
+
+      stage.on('mouseup touchend', () => {
+        // no nothing if we didn't start selection
+        if (!selectionRectangle.visible()) {
+          return;
+        }
+        // update visibility in timeout, so we can check it in click event
+        setTimeout(() => {
+          selectionRectangle.visible(false);
+          layer.batchDraw();
+        });
+
+        var shapes = stage.find('.rect').toArray();
+        var box = selectionRectangle.getClientRect();
+        var selected = shapes.filter((shape) =>
+          Konva.Util.haveIntersection(box, shape.getClientRect())
+        );
+        tr.nodes(selected);
+        layer.batchDraw();
+      });
+
+      // clicks should select/deselect shapes
+      stage.on('click tap', function (e) {
+        // if we are selecting with rect, do nothing
+        if (selectionRectangle.visible()) {
+          return;
+        }
+
+        // if click on empty area - remove all selections
+        if (e.target === stage) {
+          tr.nodes([]);
+          layer.draw();
+          return;
+        }
+
+        // do nothing if clicked NOT on our rectangles
+        if (!e.target.hasName('rect')) {
+          return;
+        }
+
+        // do we pressed shift or ctrl?
+        const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
+        const isSelected = tr.nodes().indexOf(e.target) >= 0;
+
+        if (!metaPressed && !isSelected) {
+          // if no key pressed and the node is not selected
+          // select just one
+          tr.nodes([e.target]);
+        } else if (metaPressed && isSelected) {
+          // if we pressed keys and node was selected
+          // we need to remove it from selection:
+          const nodes = tr.nodes().slice(); // use slice to have new copy of array
+          // remove node from array
+          nodes.splice(nodes.indexOf(e.target), 1);
+          tr.nodes(nodes);
+        } else if (metaPressed && !isSelected) {
+          // add the node into selection
+          const nodes = tr.nodes().concat([e.target]);
+          tr.nodes(nodes);
+        }
+        layer.draw();
+      });
   }
 
    drawSofa() {
@@ -470,12 +565,6 @@ class MyCanvas {
         group.add(anchor);
       }
   
-  //----------------------- general methods --------------------------------------
-  // return center position of canvas
-  getCenterPosition(){
-    return new Point({x: this.canvasElement.clientWidth/2, y:this.canvasElement.clientHeight/2});
-  }
-
   // helper to set stroke and fill
   setStrokeAndFill(item){
     item.strokeWidth = this.strokeWidth;
